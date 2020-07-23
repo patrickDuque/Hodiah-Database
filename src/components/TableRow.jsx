0f866ffa02d9075519f80f4e375c8107024cf1f0
Patrick Duque <patpatduque26@gmail.com>
2020-07-23 21:26:06 +0800

    capitalize items and store name

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default props => (
   <tr>
-    <td>{props.itemName}</td>
-    <td>{props.storeName}</td>
+    <td className='TableRow'>{props.itemName}</td>
+    <td className='TableRow'>{props.storeName}</td>
     <td>&#8369;{props.buyingPrice}</td>
     <td>&#8369;{props.sellingPrice}</td>
     <td>{props.contactNumber}</td>
